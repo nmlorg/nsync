8056ea1d95197e82ae3b8722dc77e2406257c80c
Daniel Reed <nmlorg@gmail.com>
2024-09-16 14:26:31 -0700

    Show how feeding data back into `await` works (by reading data from the network).

--- examples/async3.py
+++ examples/async3.py
@@ -1,5 +1,6 @@
-"""A version of async1.py that shows how `await` works."""
+"""A version of async2.py that shows how feeding data back into `await` works."""
 
+import socket
 import time
 
 start = time.time()
@@ -23,37 +24,62 @@ class SleepToken:
         log('SleepToken.__await__ yield self:', ret)
 
 
-async def async_addone(a):
-    """Add 1 to a."""
+class ReadToken:
+    """Token to tell the event loop to resume a coroutine after reading data from a socket."""
 
-    log('async_addone', a)
-    log('async_addone await SleepToken(1.1)')
+    def __init__(self, sock):
+        log('ReadToken.__init__', self, sock)
+        self.sock = sock
+
+    def __await__(self):
+        log('ReadToken.__await__', self)
+        log('ReadToken.__await__ yield self')
+        ret = yield self
+        log('ReadToken.__await__ yield self:', ret)
+        return ret
+
+
+async def async_read():
+    """Read data from a socket."""
+
+    log('async_read')
+    log('async_read await SleepToken(1.1)')
     ret = await SleepToken(1.1)
-    log('async_addone await SleepToken(1.1):', ret)
-    return a + 1
+    log('async_read await SleepToken(1.1):', ret)
+    sock = socket.create_connection(('httpbin.org', 80))
+    sock.sendall(b'GET /get HTTP/1.0\r\n\r\n')
+    log('async_read await ReadToken')
+    ret = await ReadToken(sock)
+    log('async_read await ReadToken:', ret)
+    return ret
 
 
 def sync_await(coroutine):
     """Run the coroutine manually, returning its value; equivalent to `await coroutine`."""
 
     log('sync_await', coroutine)
+    value = None
     while True:
         log('sync_await coroutine.send')
 
         try:
-            ret = coroutine.send(None)
+            ret = coroutine.send(value)
         except StopIteration as e:
             log('sync_await StopIteration', e)
             return e.value
 
         log('sync_await coroutine.send:', ret)
+        value = None
         if isinstance(ret, SleepToken):
             log('sync_await time.sleep', ret.delay)
             time.sleep(ret.delay)
+        elif isinstance(ret, ReadToken):
+            log('sync_await', ret.sock, 'read')
+            value = ret.sock.recv(65536)
 
 
 def main():  # pylint: disable=missing-function-docstring
-    ret = sync_await(async_addone(5))
+    ret = sync_await(async_read())
     log('sync_await:', ret)
 
 
