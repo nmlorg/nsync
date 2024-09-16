2493a9d784d29d71c5e080d09a53041cdcd6ecbf
Daniel Reed <nmlorg@gmail.com>
2024-09-16 16:34:37 -0700

    Show how running multiple coroutines in parallel (by explicitly gathering rather than tasking) works.

--- examples/async4.py
+++ examples/async4.py
@@ -1,5 +1,6 @@
-"""A version of async2.py that shows how feeding data back into `await` works."""
+"""A version of async3.py that shows how running multiple coroutines in parallel works."""
 
+import select
 import socket
 import time
 
@@ -39,6 +40,21 @@ class ReadToken:
         return ret
 
 
+class GatherToken:
+    """Token to tell the event loop to resume after a list of 1 or more tokens finishes."""
+
+    def __init__(self, *tokens):
+        log('GatherToken.__init__', self, tokens)
+        self.tokens = tokens
+
+    def __await__(self):
+        log('GatherToken.__await__', self)
+        log('GatherToken.__await__ yield self')
+        ret = yield self
+        log('GatherToken.__await__ yield self:', ret)
+        return ret
+
+
 async def async_read():
     """Read data from a socket."""
 
@@ -48,15 +64,24 @@ async def async_read():
     ret = await SleepToken(1.1)
     log('async_read await SleepToken(1.1):', ret)
 
-    log('async_read socket.create_connection')
-    sock = socket.create_connection(('httpbin.org', 80))
-    log('async_read sock.sendall')
-    sock.sendall(b'GET /get HTTP/1.0\r\n\r\n')
-    log('async_read await ReadToken')
-    ret = await ReadToken(sock)
-    log('async_read await ReadToken:', ret)
+    sleep15 = SleepToken(1.5)
 
-    return ret
+    log('async_read socket.create_connection')
+    sock1 = socket.create_connection(('httpbin.org', 80))
+    log('async_read sock.sendall')
+    sock1.sendall(b'GET /get HTTP/1.0\r\n\r\n')
+    read1 = ReadToken(sock1)
+
+    log('async_read socket.create_connection')
+    sock2 = socket.create_connection(('httpbin.org', 80))
+    log('async_read sock.sendall')
+    sock2.sendall(b'GET /get HTTP/1.0\r\n\r\n')
+    read2 = ReadToken(sock2)
+
+    log('async_read await GatherToken')
+    read1ret, _, read2ret = await GatherToken(read1, sleep15, read2)
+    log('async_read await GatherToken:', read1ret, read2ret)
+    return read1ret, read2ret
 
 
 def sync_await(coroutine):
@@ -74,13 +99,47 @@ def sync_await(coroutine):
             return e.value
 
         log('sync_await coroutine.send:', ret)
-        value = None
-        if isinstance(ret, SleepToken):
-            log('sync_await time.sleep', ret.delay)
-            time.sleep(ret.delay)
-        elif isinstance(ret, ReadToken):
-            log('sync_await', ret.sock, 'read')
-            value = ret.sock.recv(65536)
+        if isinstance(ret, GatherToken):
+            value = process_tokens(ret.tokens)
+        else:
+            value = process_tokens([ret])[0]
+
+
+def process_tokens(tokens):
+    """Read from all ReadTokens and sleep until the longest SleepToken."""
+
+    log('process_tokens', tokens)
+    responses = [None] * len(tokens)
+    delay = 0
+    for token in tokens:
+        if isinstance(token, SleepToken):
+            if token.delay > delay:
+                delay = token.delay
+    stoptime = delay and time.time() + delay
+
+    while True:
+        rlist = []
+        for i, token in enumerate(tokens):
+            if isinstance(token, ReadToken) and responses[i] is None:
+                rlist.append(token.sock)
+        log('process_tokens rlist =', rlist)
+
+        if not rlist:
+            break
+
+        rlist, _, _ = select.select(rlist, [], [])
+        log('process_tokens select =', rlist)
+        for sock in rlist:
+            for i, token in enumerate(tokens):
+                if isinstance(token, ReadToken) and token.sock is sock:
+                    responses[i] = sock.recv(65536)
+                    break
+
+    if (remaining := stoptime - time.time()) > 0:
+        log('process_tokens remaining =', remaining)
+        time.sleep(remaining)
+
+    return responses
 
 
 def main():  # pylint: disable=missing-function-docstring
