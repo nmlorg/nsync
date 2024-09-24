403e5d6fa60287ab71fc4a84a0385af5a741c6d2
Daniel Reed <nmlorg@gmail.com>
2024-09-24 07:56:25 -0700

    Initial implementation of basic coro-token-coro-token logic (see #1), as in:
      async def f():
          await GatherToken(g(SleepToken(.1)))
    
      async def g(token):
          await token
    
    This handles sleeping, and has the framework for network I/O, but does not actually select/read/finalize yet (`process_tokens` needs to be rewritten to accept a _WaitingFor instead of a list of tokens).

--- examples/async5.py
+++ examples/async5.py
@@ -41,11 +41,11 @@ class ReadToken(_BaseToken):
 
 
 class GatherToken(_BaseToken):
-    """Token to tell the event loop to resume after a list of 1 or more tokens finishes."""
+    """Token to tell the event loop to resume after a list of 1 or more awaitables finishes."""
 
-    def __init__(self, *tokens):
-        log(self, tokens)
-        self.tokens = tokens
+    def __init__(self, *awaitables):
+        log(self, awaitables)
+        self.awaitables = awaitables
 
 
 async def coro_token():
@@ -137,25 +137,160 @@ async def coro_token_coro_token():
     return 'coro_token_coro_token final value'
 
 
+class _BaseWrapper:
+    finalized = False
+    value = None
+
+    def __init__(self, parent):
+        self._parent = parent
+
+    def finalize(self, value):
+        """Mark this awaitable as finalized and notify anything waiting for it."""
+
+        log('self =', self, 'value =', value)
+        self.value = value
+        self.finalized = True
+        if self._parent:
+            self._parent.step()
+
+    def step(self):
+        """Check whether this awaitable is still waiting, and perform as much work as possible."""
+
+        log('self =', self)
+
+    def get_waiting_for(self):
+        """Return a _WaitingFor of everything this is waiting for (directly or indirectly)."""
+
+        raise NotImplementedError()
+
+    @staticmethod
+    def wrap(parent, awaitable):
+        """Wrap the given awaitable in the appropriate _BaseWrapper subclass."""
+
+        log('parent =', parent, 'awaitable =', awaitable)
+        if inspect.iscoroutine(awaitable):
+            return CoroutineWrapper(parent, awaitable)
+        if isinstance(awaitable, ReadToken):
+            return ReadWrapper(parent, awaitable.sock)
+        if isinstance(awaitable, SleepToken):
+            return SleepWrapper(parent, awaitable.delay)
+        if isinstance(awaitable, GatherToken):
+            return GatherWrapper(parent, awaitable.awaitables)
+        raise NotImplementedError()
+
+
+class _WaitingFor:
+
+    def __init__(self, *, readers=(), sleeper=None):
+        log('self =', self, 'readers =', readers, 'sleeper =', sleeper)
+        self.readers = readers
+        self.sleeper = sleeper
+
+
+class CoroutineWrapper(_BaseWrapper):
+    """An instance of a call to an async def function."""
+
+    def __init__(self, parent, coro):
+        log('self =', self, 'parent =', parent, 'coro =', coro)
+        super().__init__(parent)
+        self._coro = coro
+        self._send(None)
+
+    _waiting_for = None
+
+    def step(self):
+        log('self =', self)
+        if self._waiting_for.finalized:
+            value = self._waiting_for.value
+            self._waiting_for = None
+            self._send(value)
+
+    def _send(self, value):
+        log('self =', self, 'value =', value)
+        try:
+            waiting_for = self._coro.send(value)
+        except StopIteration as e:
+            self.finalize(e.value)
+        else:
+            self._waiting_for = _BaseWrapper.wrap(self, waiting_for)
+
+    def get_waiting_for(self):
+        log('self =', self)
+        return self._waiting_for.get_waiting_for()
+
+
+class ReadWrapper(_BaseWrapper):
+    """An attempt to await a ReadToken (read data from a network socket)."""
+
+    def __init__(self, parent, sock):
+        log('self =', self, 'parent =', parent, 'sock =', sock)
+        super().__init__(parent)
+        self._sock = sock
+
+    def get_waiting_for(self):
+        log('self =', self)
+        return _WaitingFor(readers=[self])
+
+
+class SleepWrapper(_BaseWrapper):
+    """An attempt to await a SleepToken (pause execution until an amount of time has passed)."""
+
+    def __init__(self, parent, delay):
+        log('self =', self, 'parent =', parent, 'delay =', delay)
+        super().__init__(parent)
+        self.deadline = time.time() + delay
+
+    def get_waiting_for(self):
+        log('self =', self)
+        return _WaitingFor(sleeper=self)
+
+
+class GatherWrapper(_BaseWrapper):
+    """An attempt to await a GatherToken (1 or more other awaitables)."""
+
+    def __init__(self, parent, awaitables):
+        log('self =', self, 'parent =', parent, 'awaitables =', awaitables)
+        super().__init__(parent)
+        self._awaitables = [_BaseWrapper.wrap(self, awaitable) for awaitable in awaitables]
+
+    def step(self):
+        log('self =', self)
+        values = []
+        for awaitable in self._awaitables:
+            if not awaitable.finalized:
+                return
+            values.append(awaitable.value)
+        self._awaitables = None
+        self.finalize(values)
+
+    def get_waiting_for(self):
+        log('self =', self)
+        readers = []
+        sleeper = None
+        for awaitable in self._awaitables:
+            waiting_for = awaitable.get_waiting_for()
+            readers.extend(waiting_for.readers)
+            if sleeper is None or (waiting_for.sleeper is not None and
+                                   sleeper.deadline > waiting_for.sleeper.deadline):
+                sleeper = waiting_for.sleeper
+        return _WaitingFor(readers=readers, sleeper=sleeper)
+
+
 def sync_await(coroutine):
     """Run the coroutine manually, returning its value; equivalent to `await coroutine`."""
 
     log('coroutine =', coroutine)
-    value = None
-    while True:
-        log('ret = coroutine.send', value)
+    coro = _BaseWrapper.wrap(None, coroutine)
 
-        try:
-            ret = coroutine.send(value)
-        except StopIteration as e:
-            log('StopIteration:', e)
-            return e.value
-
-        log('ret =', ret)
-        if isinstance(ret, GatherToken):
-            value = process_tokens(ret.tokens)
-        else:
-            value = process_tokens([ret])[0]
+    while not coro.finalized:
+        waiting_for = coro.get_waiting_for()
+        log('readers =', waiting_for.readers, 'sleeper =', waiting_for.sleeper)
+        waiting_for.sleeper.finalize(time.sleep(waiting_for.sleeper.deadline - time.time()))
+        #if isinstance(ret, GatherToken):
+        #    value = process_tokens(ret.awaitables)
+        #else:
+        #    value = process_tokens([ret])[0]
+    return coro.value
 
 
 def process_tokens(tokens):
